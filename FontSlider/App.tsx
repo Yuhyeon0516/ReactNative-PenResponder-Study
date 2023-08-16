65f9b14db6027881f69d0c6eb2b7f650b64820cd
Yuhyeon0516 <rladbgus0516@naver.com>
2023-08-16 18:28:31 +0900

    created Font slide using PanResponder & Animated

---
+++ FontSlider/App.tsx
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import FontSlider from './src/FontSlider';
+
+function App(): JSX.Element {
+  return <FontSlider />;
+}
+
+export default App;
